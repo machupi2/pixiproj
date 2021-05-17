7bc32e6093aad53e0c1a4168d7910ed912b9b6b0
YasuakiAsano <maroyakamaronii@gmail.com>
2021-05-17 17:29:14 +0900

    eslint が webpack.config.js にも掛かるのを抑制

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,82 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require("path");
+const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const { SourceMapDevToolPlugin } = require("webpack");
+const { SourceMapDevToolPlugin } = require('webpack');
 
-const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV == 'production';
 
-const stylesHandler = "style-loader";
+const stylesHandler = 'style-loader';
 
 const config = {
-  entry: "./src/index.ts",
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Pixi.js Demo'
-    }),
-    new CopyPlugin({
-        patterns: [
-            { from: 'src/assets', to: 'assets' },
-        ]
-    }),
-    new SourceMapDevToolPlugin({
-      filename: "[file].map"
-    }),
+    entry: './src/index.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Pixi.js Demo',
+        }),
+        new CopyPlugin({
+            patterns: [{ from: 'src/assets', to: 'assets' }],
+        }),
+        new SourceMapDevToolPlugin({
+            filename: '[file].map',
+        }),
 
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
-      },
-      {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    stylesHandler,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+    performance: { hints: false }, // WARNING in *** size limitが発生するので無視
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
 };
 
 module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-    config.devtool="eval-source-map";
-  }
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+        config.devtool = 'eval-source-map';
+    }
 
-  return config;
+    return config;
 };
